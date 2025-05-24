bbcfa319b7685f626f005ed443d287010ecb12bf
Matheus <matheusribasdelima@gmail.com>
2025-05-24 01:11:42 -0300

    alteração sql novamente

--- apiAlugueis/obj/Debug/net9.0/apiAlugueis.AssemblyInfo.cs
+++ apiAlugueis/obj/Debug/net9.0/apiAlugueis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apiAlugueis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb1af72c53762a6fa420c1ce1c62d46e8cb76bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a7d87c119a8ff09ceeb76a00655e13299fae66")]
 [assembly: System.Reflection.AssemblyProductAttribute("apiAlugueis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apiAlugueis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
