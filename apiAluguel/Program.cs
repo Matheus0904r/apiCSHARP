8ccc0b4b34a10c70a02cf86ad4a1a8f4814782bc
SimpleNBO <simplenbo@gmail.com>
2025-05-24 02:38:07 -0300

    alo

--- apiAluguel/Program.cs
+++ apiAluguel/Program.cs
@@ -1,6 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using apiCSHARP.apiAluguel.Models;
+using apiCSHARP.apiAluguel.Rotas;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Locatarios>(options => options.UseSqlite("Data Source=locacoes.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+var app = builder.Build();
+app.UseCors("AllowAll");
+
+app.MapGetRoutes();
+app.MapPostRoutes();
+app.MapDeleteRoutes();
+
+PopularBancoDeDados(app);
 
 app.Run();
+
+void PopularBancoDeDados(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var Dados = scope.ServiceProvider.GetRequiredService<Locatarios>();
+
+    Dados.Database.Migrate();
+
+    if (Dados.Locacoes.Any()) { return; }
+
+    var locacoesIniciais = new List<Pessoa>
+    {
+        new() {}
+    };
+
+    Dados.Locacoes.AddRange(locacoesIniciais);
+    Dados.SaveChanges();
+}
